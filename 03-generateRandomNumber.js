65ff077f191b6dbd764f3f31cc08fbffb659a5f3
vishnu2815 <vishnumathariya28@gmail.com>
2026-03-24 17:43:26 +0530

    Add today's work

---
+++ 03-generateRandomNumber.js
@@ -0,0 +1,27 @@
+
+// function getRandomNumber(){
+//     return Math.ceil (Math.random() * 10)
+// }
+
+
+
+// for (let i=1; i<100; i++){
+//     console.log(getRandomNumber());
+// }
+
+
+
+// generate random number btn min and max 
+
+
+let min=5;
+let max=20;
+
+let  getRandomNumber = Math.floor(Math.random() * (max-min + 1 )) + min
+
+
+
+console.log(getRandomNumber)
+console.log(getRandomNumber)
+console.log(getRandomNumber)
+console.log(getRandomNumber)
