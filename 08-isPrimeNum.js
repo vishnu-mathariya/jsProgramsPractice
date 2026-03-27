92e1193aeb72ae9a3289388b0bbff326e976b21c
vishnu2815 <vishnumathariya28@gmail.com>
2026-03-27 17:35:48 +0530

    Updated code

--- 08-isPrimeNum.js
+++ 08-isPrimeNum.js
@@ -1,18 +1,44 @@
-const isPrimeNum = (num)=>{
-    let isPrime = true
-if(num === 1 || num < 1)
-    return "Num is not prime"
-else {
-    for (let i=2; i<=num /2; i++){
-        if(num % i === 0)
-            isPrime =false
+// const isPrimeNum = (num)=>{
+//     let isPrime = true
+// if(num === 1 || num < 1)
+//     return "Num is not prime"
+// else {
+//     for (let i=2; i<=num /2; i++){
+//         if(num % i === 0)
+//             isPrime =false
+//     }
+//     if(isPrime)
+//         return "num is prime"
+//     else
+//         return "num is not prime"
+
+// }
+// }
+
+// console.log(isPrimeNum(2))
+
+
+
+
+
+
+// -------------------------------------------------
+
+
+
+
+const  isPrimeNum = (num)=>{
+    if(num <=1 ) 
+        return false;
+
+    for (let i =2; i<num; i++){
+        if(num % i ===0) 
+            return false;
+
     }
-    if(isPrime)
-        return "num is prime"
-    else
-        return "num is not prime"
+
+    return true;
 
 }
-}
 
-console.log(isPrimeNum(2))
+console.log(isPrimeNum(10))
