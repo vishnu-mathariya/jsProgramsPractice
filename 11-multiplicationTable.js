6e65095198676bdab23c377213dddb6a717c8ee7
vishnu2815 <vishnumathariya28@gmail.com>
2026-03-30 13:36:59 +0530

    today's work

---
+++ 11-multiplicationTable.js
@@ -0,0 +1,21 @@
+// const multiplicationTable = (num) => {
+//     for (let i=1; i <= 10; i++){
+//         console.log(`${num} * ${i} = ${num * i}`);
+
+    
+//     }
+// }
+// (multiplicationTable(5))
+
+
+
+
+
+const multiplicationTable = (num) => {
+    for (let i=1; i <= 10; i++){
+        console.log(`${num} * ${i} = ${num * i}`);
+        
+    }
+}
+
+multiplicationTable(10)
