e42673c7a977f268a579f6142d761c22ceb28cbb
vishnu2815 <vishnumathariya28@gmail.com>
2026-04-01 14:14:00 +0530

    Update code in file

--- jsLogicBuilding/4-checkNumOddOrEven.js
+++ jsLogicBuilding/4-checkNumOddOrEven.js
@@ -1,9 +1,19 @@
-const isNumOddEven = (num) => {
-    if(num % 2===0)
-        return "Even num"
-    else 
-        return "Odd num"
+// const isNumOddEven = (num) => {
+//     if(num % 2===0)
+//         return "Even num"
+//     else 
+//         return "Odd num"
 
+// }
+
+// console.log(isNumOddEven(1));
+
+
+
+// By using different way 
+
+const isNumOddOrEven = (num) =>{
+    return num % 2 === 0 ? "even" : "odd"
 }
 
-console.log(isNumOddEven(1));
+console.log(isNumOddOrEven(3));
