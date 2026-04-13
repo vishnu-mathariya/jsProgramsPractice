50c13569b8bcb48d9e9d5d4584345eed978db02c
vishnu2815 <vishnumathariya28@gmail.com>
2026-04-13 16:54:38 +0530

    Updated code

--- jsLogicBuilding/11-findMaxNum.js
+++ jsLogicBuilding/11-findMaxNum.js
@@ -13,3 +13,26 @@ const findMaxNum = (inputArr) => {
 };
 
 findMaxNum([1, 2, 3]);
+
+////////////////////////////////////
+
+const findMaxNum2 =() => {
+
+   const maxNumber= Math.max(10,29,30)
+   console.log(maxNumber);
+   
+}
+
+findMaxNum2([]);
+
+//////////////////////////////////////////
+
+
+const findMaxNum3 =(inputArr) => {
+
+   const maxNumber= Math.max(...inputArr)
+   console.log(maxNumber);
+   
+}
+
+findMaxNum3([1,100,300]);
