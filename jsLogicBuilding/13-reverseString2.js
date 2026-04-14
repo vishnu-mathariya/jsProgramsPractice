ba83b785e7cf2eafe560a2a4c8f475bb0880f384
vishnu2815 <vishnumathariya28@gmail.com>
2026-04-14 17:20:22 +0530

    Today's work

---
+++ jsLogicBuilding/13-reverseString2.js
@@ -0,0 +1,33 @@
+const reverseStr = (inputStr) => {
+  let str = "";
+
+  for (let i = inputStr.length - 1; i >= 0; i--) {
+    str += inputStr[i];
+  }
+
+  console.log(str);
+};
+
+reverseStr("vishnu");
+
+
+
+const reverseStr2 = (inpStr) => {
+  let str2 = "";
+  for (let i = inpStr.length - 1; i >= 0; i--) {
+    str2 += inpStr[i];
+  }
+  console.log(str2);
+};
+
+reverseStr2("Jassi");
+
+
+
+const reverseStr3 = (inpStr3) =>{
+    console.log(inpStr3.split("").reverse().join(""));
+    
+
+}
+
+reverseStr3("Kunj")
