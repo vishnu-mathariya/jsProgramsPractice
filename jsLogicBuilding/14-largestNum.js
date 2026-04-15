2ff3f8fbdadd2838d1a2d9b643cf4925d655023b
vishnu2815 <vishnumathariya28@gmail.com>
2026-04-15 16:32:23 +0530

    updated code

--- jsLogicBuilding/14-largestNum.js
+++ jsLogicBuilding/14-largestNum.js
@@ -1,14 +1,43 @@
-const findLargestNum =(inputArr) => {
+// const findLargestNum =(inputArr) => {
+//     let maxNum =inputArr[0]
+//     for(let i=1; i <= inputArr.length; i++ ){
+//         if(inputArr[i] > maxNum){
+//         maxNum= inputArr[i]
+//         }
+//     }
+//     console.log(maxNum);
+
+// }
+
+// findLargestNum([1,2,3])
+
+// \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
+
+// const findMaxNum = (inputNum) => {
+//   let maxNum = inputNum[0];
+
+//   for (let i = 1; i < inputNum.length; i++) {
+//     if (inputNum[i] > maxNum) {
+//       maxNum = inputNum[i];
+//     }
+//   }
+//   console.log(maxNum);
+// };
+
+// findMaxNum([3, 4, 2]);
+
+
+
+
+const findMaxNum =(inputArr) =>{
     let maxNum =inputArr[0]
-    for(let i=1; i <= inputArr.length; i++ ){
+    for(let i=1; i< inputArr.length;i++){
         if(inputArr[i] > maxNum){
-        maxNum= inputArr[i]
+            maxNum=inputArr[i]
         }
     }
     console.log(maxNum);
     
 }
 
-findLargestNum([1,2,3])
-
-
+findMaxNum([10,20,30])
