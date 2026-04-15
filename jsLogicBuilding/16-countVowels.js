92475a26754b507f1ecfa2f61c20e9c39203392c
vishnu2815 <vishnumathariya28@gmail.com>
2026-04-15 19:09:46 +0530

    Today's work

---
+++ jsLogicBuilding/16-countVowels.js
@@ -0,0 +1,49 @@
+const countVowels = (inpStr) =>{
+    let count =0
+    for(let i=0; i< inpStr.length; i++){
+        let char =inpStr[i].toLowerCase();
+
+
+        if(
+            char === "a"||  char === "e" ||  char === "i" ||  char === "o" ||  char === "u"
+        ){
+            count ++;
+        }
+    }
+
+    return count;
+
+}
+
+console.log(countVowels("Vishnu"));
+
+
+// =================================================
+
+
+const  countVowels2  = (str) =>{
+    let count =0
+    let vowels ="aeiou";
+
+    for(let i=0; i<str.length; i++ ){
+        if(vowels.includes(str[i].toLowerCase())){
+            count++
+        }
+    }
+    return count;
+}
+
+console.log(countVowels2("bharat"))
+
+
+// ===============================
+
+
+
+const countVowels3 = (str) =>{
+    return str
+    .toLowerCase()
+    .split("")
+    .filter((char) => "aeiou".includes(char)).length
+}
+console.log(countVowels3("jassi"));
