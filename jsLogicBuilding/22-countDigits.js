86f4ae58abd0103bbd844a43e4483056965b2688
vishnu2815 <vishnumathariya28@gmail.com>
2026-04-20 15:23:11 +0530

    updated code

--- jsLogicBuilding/22-countDigits.js
+++ jsLogicBuilding/22-countDigits.js
@@ -1,17 +1,27 @@
-const countDigits = (inpDig) =>{
-    let count = 0
+const countDigits = (inpDig) => {
+  let count = 0;
 
-    let str = inpDig.toString()
-    for(let i=0; i < str.length; i++){
-        count ++
-    }
+  let str = inpDig.toString();
+  for (let i = 0; i < str.length; i++) {
+    count++;
+  }
 
-    return count
-
-}
-
+  return count;
+};
 
 console.log(countDigits(123467));
 
-
 ///////////////////////////////////////
+
+const countDig = (num) => {
+  let count = 0;
+  let str = num.toString();
+
+  for (let i = 0; i < str.length; i++) {
+    count++;
+  }
+
+  return count;
+};
+
+console.log(countDig(1234566610101));
