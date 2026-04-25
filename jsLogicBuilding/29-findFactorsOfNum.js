399e7f5aa893547365801280dba35ce75fdbdb90
vishnu2815 <vishnumathariya28@gmail.com>
2026-04-25 15:08:43 +0530

    Updated code

--- jsLogicBuilding/29-findFactorsOfNum.js
+++ jsLogicBuilding/29-findFactorsOfNum.js
@@ -1,11 +1,16 @@
 const findFactorsOfNum = (num) => {
+
+    if(num < 1){
+        throw new Error("Input num should be greater than 1")
+    }
   let result = [];
   for (let i = 1; i <= num; i++) {
     if (num % i === 0) {
       result.push(i);
     }
+
   }
   return result;
 };
 
-console.log(findFactorsOfNum(14));
+console.log(findFactorsOfNum(6));
