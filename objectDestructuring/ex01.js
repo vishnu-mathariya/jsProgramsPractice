5539abe81ab56762b75298c938ddf7fa6ab5dec4
vishnu2815 <vishnumathariya28@gmail.com>
2026-05-04 13:56:35 +0530

    Today's work

---
+++ objectDestructuring/ex01.js
@@ -0,0 +1,27 @@
+const user ={
+    name: "Vishnu",
+    age: 25
+}
+
+const {name, age} = user
+
+console.log({name}, {age});
+
+
+
+/////////////////////////////
+
+const products = {
+    title: "Laptop",
+    price: 50000,
+    brand: "Lenovo"
+}
+
+const {title, price, brand} = products
+
+console.log(`Title : ${title}`);
+console.log(`Price : ${price}`);
+console.log(`Brand : ${brand}`);
+
+
+
